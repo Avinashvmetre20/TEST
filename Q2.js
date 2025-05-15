913ed38d6d3230a3716e0fd5dc5427bda3ff79e1
Avinashvmetre20 <avinashvmetre20@gmail.com>
2025-05-15 11:44:40 +0530

    secound ans done

--- Q2.js
+++ Q2.js
@@ -1,8 +1,5 @@
 let requests = [["excavator", 50000], ["bulldozer", 70000]]
 let sellers = [["excavator", 45000], ["bulldozer", 68000], ["excavator", 48000]]
-
-
-
 function question(requests,sellers){
     let result = []
     for(let i=0;i<requests.length;i++){
@@ -15,13 +12,8 @@ function question(requests,sellers){
             }
         }
         result.push(min)
-        
     }
     return result
 }
-
-
-
-
 let result = question(requests,sellers);
 console.log(result)

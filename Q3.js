cd91d0377d769fcf05c80609bb9107954c1416af
Avinashvmetre20 <avinashvmetre20@gmail.com>
2025-05-15 12:07:07 +0530

    3rd ans done

--- Q3.js
+++ Q3.js
@@ -1,60 +1,17 @@
 class FenwickTree {
-  constructor(size) {
-    this.tree = new Array(size + 1).fill(0);
-  }
-
-  update(index, value) {
-    while (index < this.tree.length) {
-      this.tree[index] += value;
-      index += index & -index;
-    }
-  }
-
-  query(index) {
-    let sum = 0;
-    while (index > 0) {
-      sum += this.tree[index];
-      index -= index & -index;
-    }
-    return sum;
-  }
-
-  rangeQuery(left, right) {
-    return this.query(right) - this.query(left - 1);
-  }
+  constructor(n) { this.tree = Array(n + 1).fill(0); }
+  update(i, v) { while (i < this.tree.length) this.tree[i] += v, i += i & -i; }
+  query(i) { let sum = 0; while (i > 0) sum += this.tree[i], i -= i & -i; return sum; }
+  range(l, r) { return this.query(r) - this.query(l - 1); }
 }
 
-function processMaintenanceLogs(maintenance_logs, queries) {
-  const uniqueDates = [...new Set(maintenance_logs.map(log => log[1]).concat(
-    ...queries.map(q => [q[0], q[1]])
-  ))].sort();
-  const dateToIndex = {};
-  uniqueDates.forEach((date, idx) => {
-    dateToIndex[date] = idx + 1;
-  });
-
-  const fenwick = new FenwickTree(uniqueDates.length);
-  for (let [_, date, cost] of maintenance_logs) {
-    fenwick.update(dateToIndex[date], cost);
-  }
-  const results = [];
-  for (let [start, end] of queries) {
-    const left = dateToIndex[start];
-    const right = dateToIndex[end];
-    results.push(fenwick.rangeQuery(left, right));
-  }
-
-  return results;
+function maintenanceCost(logs, queries) {
+  const dates = [...new Set(logs.map(x => x[1]).concat(...queries.flat()))].sort();
+  const map = Object.fromEntries(dates.map((d, i) => [d, i + 1]));
+  const bit = new FenwickTree(dates.length);
+  for (let [_, d, c] of logs) bit.update(map[d], c);
+  return queries.map(([s, e]) => bit.range(map[s], map[e]));
 }
-const maintenance_logs = [
-  [101, "2024-01-01", 500],
-  [102, "2024-01-10", 300],
-  [101, "2024-01-15", 700]
-];
-
-const queries = [
-  ["2024-01-01", "2024-01-10"],
-  ["2024-01-01", "2024-01-15"]
-];
-
-console.log(processMaintenanceLogs(maintenance_logs, queries));
+const logs = [[101, "2024-01-01", 500], [102, "2024-01-10", 300], [101, "2024-01-15", 700]];
+const queries = [["2024-01-01", "2024-01-10"], ["2024-01-01", "2024-01-15"]];
+console.log(maintenanceCost(logs, queries));
